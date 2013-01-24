66a794483ab66a8cdc465c67d3441e7832793bab
Keith Burdis <keith@burdis.org>
2013-01-24 12:34:50 +0000

    Add domain parameter and deliver idmapd.conf.  Ensure /etc/pam_ldap.conf, /etc/openldap/ldap.conf and /etc/ldap.conf all have the same content as different tools look for them in different places.

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,6 +1,7 @@
 class pamldap::config (
   $base_dn,
   $uris,
+  $domain,
 ) {
   $uris_space = join($uris, ' ')
   $uris_comma = join($uris, ',')
@@ -23,17 +24,30 @@ class pamldap::config (
     require => Class['pamldap::install'],
     notify  => Class['pamldap::service'],
   }
-  file { [ '/etc/ldap.conf', '/etc/openldap/ldap.conf' ]:
+  file { '/etc/ldap.conf':
     ensure  => present,
     mode    => '0444',
     content => template('pamldap/ldap.conf.erb'),
     require => Class['pamldap::install'],
     notify  => Class['pamldap::service'],
   }
+  file { [ '/etc/pam_ldap.conf', '/etc/openldap/ldap.conf' ]:
+    ensure  => link,
+    target  => '/etc/ldap.conf',
+    require => File['/etc/ldap.conf'],
+    notify  => Class['pamldap::service'],
+  }
   file { '/etc/sssd/sssd.conf':
     ensure  => present,
     mode    => '0600',
     content => template('pamldap/sssd.conf.erb'),
+    require => Class['pamldap::install'],
+    notify  => Class['pamldap::service'],
+  }
+  file { '/etc/idmapd.conf':
+    ensure  => present,
+    mode    => '0644',
+    content => template('pamldap/idmapd.conf.erb'),
     require => Class['pamldap::install'],
     notify  => Class['pamldap::service'],
   }
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,11 @@
 class pamldap (
   $base_dn = hiera('pamldap::base_dn'),
+  $domain  = hiera('pamldap::domain'),
   $uris    = hiera('pamldap::uris'),
 ) {
   class { 'pamldap::config':
     base_dn => $base_dn,
+    domain  => $domain,
     uris    => $uris,
   }
   include pamldap::install
