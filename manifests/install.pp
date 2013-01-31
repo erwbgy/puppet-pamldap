ec3c24c08d1bc654133c972448eea87f825222ec
Markus Heberling <markus@tisoft.de>
2013-01-31 19:49:04 +0100

    openldap-clients is ldap-utils on ubuntu

--- manifests/install.pp
+++ manifests/install.pp
@@ -6,14 +6,14 @@ class pamldap::install::common {
   if ! defined(Package['sssd']) {
     package { 'sssd':  ensure => installed }
   }
-  if ! defined(Package['openldap-clients']) {
-    package { 'openldap-clients':  ensure => installed }
-  }
 }
 
 class pamldap::install::redhat inherits pamldap::install::common {
   if ! defined(Package['sssd-client']) {
     package { 'sssd-client':  ensure => installed }
+  }
+  if ! defined(Package['openldap-clients']) {
+    package { 'openldap-clients':  ensure => installed }
   }
 }
 
@@ -27,5 +27,8 @@ class pamldap::install::ubuntu inherits pamldap::install::common {
   }
   if ! defined(Package['libpam-sss']) {
     package { 'libpam-sss':  ensure => installed }
+  }
+  if ! defined(Package['ldap-utils']) {
+    package { 'ldap-utils':  ensure => installed }
   }
 }
