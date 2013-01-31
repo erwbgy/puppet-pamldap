5c26e909a1f5cce4b842c06d1266e2bc5f043e34
Markus Heberling <markus@tisoft.de>
2013-01-31 19:36:40 +0100

    install correct packages on ubuntu

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,11 +1,31 @@
 class pamldap::install {
+  include "pamldap::install::$operatingsystem"
+}
+
+class pamldap::install::common {
   if ! defined(Package['sssd']) {
     package { 'sssd':  ensure => installed }
   }
+  if ! defined(Package['openldap-clients']) {
+    package { 'openldap-clients':  ensure => installed }
+  }
+}
+
+class pamldap::install::redhat inherits pamldap::install::common {
   if ! defined(Package['sssd-client']) {
     package { 'sssd-client':  ensure => installed }
   }
-  if ! defined(Package['openldap-clients']) {
-    package { 'openldap-clients':  ensure => installed }
+}
+
+class pamldap::install::centos inherits pamldap::install::redhat {
+    # No changes
+}
+
+class pamldap::install::ubuntu inherits pamldap::install::common {
+  if ! defined(Package['libnss-sss']) {
+    package { 'libnss-sss':  ensure => installed }
+  }
+  if ! defined(Package['libpam-sss']) {
+    package { 'libpam-sss':  ensure => installed }
   }
 }
